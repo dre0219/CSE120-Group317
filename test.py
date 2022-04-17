44281d8a3a4b421ff9fa85bc58671e17ed1b5552
Vaccarios <jowikarl@yahoo.com>
2022-04-17 15:51:17 -0700

    DataTable unpolished but fully working

---
+++ test.py
@@ -0,0 +1,19 @@
+# from contextlib import redirect_stderr
+# import sqlite3
+# from ast import Slice
+# from re import S
+# from flask import Flask, request, jsonify, render_template, url_for, current_app, g, redirect
+# from flask.cli import with_appcontext
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
+# from flask_restful import Api, Resource, reqparse
+# import json
+
+# app = Flask(__name__)
+# api = Api(app)
+# CORS(app)
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
