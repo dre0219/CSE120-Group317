b1ed8b38edede74dab550cff4bcfea78c1454dea
Alan Triano <alanatriano@gmail.com>
2022-04-18 20:25:55 -0700

    Modified DB to remove unnecessary tables along with queries, added future saving of composite searches to DB, frontend datatable now prints out name and address

--- app.py
+++ app.py
@@ -43,26 +43,39 @@ def save_area_to_db():
     global area_id
     global coordinates
     if request.method == 'POST':
-        data2 = request.form.to_dict()
-        # connection = get_db_connect()
-        # cursor = connection.cursor()
-        # cursor.execute('''pragma foreign_keys = ON''')
-        # connection.commit()
-        latitude1 = data2['testcoordNE[lat]']    #assign to latitude1
-        longitude1 = data2['testcoordNE[lng]']   #assign to longitude1
-        latitude2 = data2['testcoordSW[lat]']    #assign to latitude2
-        longitude2 = data2['testcoordSW[lng]']   #assign to longitude2
-        # composite_id = 0
+        data = request.form.to_dict()
+        connection = get_db_connect()
+        cursor = connection.cursor()
+        cursor.execute('''pragma foreign_keys = ON''')
+        connection.commit()
+        latitude1 = data['testcoordNE[lat]']    #assign to latitude1
+        longitude1 = data['testcoordNE[lng]']   #assign to longitude1
+        latitude2 = data['testcoordSW[lat]']    #assign to latitude2
+        longitude2 = data['testcoordSW[lng]']   #assign to longitude2
+        composite_id = 0
         # cursor.execute('''INSERT INTO areas(area_id, latitude1, longitude1, latitude2, longitude2, composite_id) VALUES(?,?,?,?,?,?)
         #                 ''', (area_id, latitude1, longitude1, latitude2, longitude2, composite_id))
-        # area_id += 0
-        # composite_id += 0
-        # connection.commit()
+        area_id += 1
+        composite_id += 0
+        connection.commit()
         coordinates = {"lat1": latitude1, "long1": longitude1, "lat2": latitude2, "long2":longitude2}
         print(coordinates)
     return ""
 
-
+@app.route('/save', methods=['POST'])
+def save_composite_to_db(composite_id, name):
+    print("saving to db")
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        connection = get_db_connect()
+        cursor = connection.cursor()
+        cursor.execute('''pragma foreign_keys = ON''')
+        connection.commit()
+        cursor.execute('''INSERT INTO areas(composite_id, composite_name, user_id) VALUES(?,?,?)
+                         ''', (composite_id, name, composite_id))
+        composite_id += 1
+        connection.commit()
+    return ""
 
 @app.route('/delete/<int:id>', methods=['DELETE'])
 def delete_area_from_db(id):
@@ -91,7 +104,7 @@ def shape_querying(latestcoords):
 
     cursor.execute('''
     SELECT *
-    FROM businesses2
+    FROM businesses
     WHERE latitude < ? AND latitude > ? AND longitude > ? AND longitude < ?
     ''', (latestcoords["lat1"],latestcoords["lat2"], latestcoords["long2"], latestcoords["long1"]))
 
