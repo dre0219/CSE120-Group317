0ec9c84c0c8b71508b9a4c27863e5f7146923c53
Vaccarios <jowikarl@yahoo.com>
2022-05-02 12:28:58 -0700

    Added Alan's changes

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from curses import tparm
 from operator import length_hint
 from flask import Flask, request, jsonify, render_template, url_for, current_app, g, redirect
@@ -56,3 +57,40 @@ def tables():
 if __name__ == "__main__":
     app.run(debug=True)
     
+=======
+from contextlib import redirect_stderr
+import sqlite3
+from ast import Slice
+from re import S
+from flask import Flask, request, jsonify, render_template, url_for, current_app, g, redirect
+from flask.cli import with_appcontext
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
+from DBClass import DBClass 
+import json
+
+# app = Flask(__name__)
+# api = Api(app)
+# CORS(app)
+dbc = DBClass()
+
+def main():
+    print("test")
+    print(dbc.load_areas_from_composite(0))
+    dbc.delete_area_from_db(7)
+    print(dbc.load_areas_from_composite(0))
+    dbc.save_composite_to_db(1, "Test.py search")
+    print("Loading composite 1's stuff before insert " + str(dbc.load_areas_from_composite(1)))
+    dict = {'testcoordNE[lat]': '37.8', 'testcoordNE[lng]': '-122.8', 'testcoordSW[lat]': '37.0', 'testcoordSW[lng]': '-122.0'}
+    dbc.save_area_to_db(dict)
+    print("Loading composite 1's stuff after insert: " + str(dbc.load_areas_from_composite(1)))
+    dbc.delete_composites_from_db(1)
+    print("Loading composite 1's stuff after delete: " + str(dbc.load_areas_from_composite(1)))
+
+if __name__ == "__main__":
+    main()
+
+
+
+>>>>>>> 79020d0d76897e70bb5b1bf44f832da20f1451ef
